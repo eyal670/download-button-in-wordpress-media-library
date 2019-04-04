ed825be95001b10b88ac238cd58e6edf663656f4
Eyal Ron <eyal670@gmail.com>
2019-04-04 10:34:04 +0300

    init commit

---
+++ download-button-in-media.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: Download Button in Media
+Description: add a download button to media image edit panel
+Author: Native-Code Web Division
+Developer: Eyal Ron
+version: 0.1
+*/
+
+/*Classic Editor*/
+add_filter('attachment_fields_to_edit', 'edit_media_custom_field', 11, 2 );
+
+function edit_media_custom_field( $form_fields, $post ) {
+    $form_fields['happy_download'] = array('input' => 'html', 'html' => '<a class="button" target="_blank" href="'.wp_get_attachment_url( $post->ID ).'" download>Download Media</a>', true  );
+    return $form_fields;
+}
